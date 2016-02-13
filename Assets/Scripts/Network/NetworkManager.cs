43e6d0fea72a9d3c49c067f41186f836f29f1a71
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-02-13 16:26:31 +0100

    First multiplayer draft

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private const string version = "v0.0.1";
+    public const string roomName = "VVR";
+    public string playerPrefabName = "Player";
+    public Transform spawnPoint;
+
+	void Start () {
+        Debug.Log("Connect");
+        PhotonNetwork.ConnectUsingSettings(version);
+	}
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby");
+        RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+        PhotonNetwork.Instantiate(playerPrefabName, spawnPoint.position, spawnPoint.rotation, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rigid;
+    private float Speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerInput();
+	}
+
+    private void playerInput()
+    {
+        float vel = Input.GetAxis("Horizontal") * Speed;
+        rigid.velocity = new Vector2(vel, rigid.velocity.y);
+    }
+}
